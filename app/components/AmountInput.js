42ab99dcde4dcb6cf0e15bca06d2519396d6e0ea
Hassan-1995 <63869278+Hassan-1995@users.noreply.github.com>
2025-01-29 15:34:12 +0500

    made some minute changes

--- app/components/AmountInput.js
+++ app/components/AmountInput.js
@@ -1,64 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet } from "react-native";
 
-import AppText from './AppText';
-import AppTextInput from './AppTextInput';
+import AppText from "./AppText";
+import AppTextInput from "./AppTextInput";
 
-function AmountInput({ title, subTitle, description, initialValue, onChangeText='', onAmount }) {
-    
+function AmountInput({
+  title,
+  subTitle,
+  description,
+  initialValue,
+  onChangeText = "",
+  onAmount,
+}) {
+  const handleText = (value) => {
+    onAmount(value);
+  };
 
-    const handleText=(value)=>{
-        onAmount(value)
-    }
-
-    return (
-        <View style={styles.container}>
-            <AppText style={styles.title}>{title}</AppText>
-            <AppText textAlign='center' style={styles.subTitle}>{subTitle}</AppText>
-            <View style={{flexDirection:'row', alignItems: 'center',justifyContent: 'space-evenly' }}>
-                <AppText style={styles.currency} >PKR</AppText>
-                <AppTextInput
-                    value={initialValue}
-                    width='60%'
-                    keyboardType="numeric"
-                    style={styles.input}
-                    onChangeText={(value)=>{
-                            handleText(value)
-                        }
-                    }
-                />
-            </View>
-            <AppText textAlign='center'>{description}</AppText>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <AppText style={styles.title}>{title}</AppText>
+      <AppText textAlign="center" style={styles.subTitle}>
+        {subTitle}
+      </AppText>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          paddingHorizontal: 20,
+        }}
+      >
+        <AppText style={styles.currency}>PKR</AppText>
+        <AppTextInput
+          value={initialValue}
+          width="70%"
+          keyboardType="numeric"
+          style={styles.input}
+          onChangeText={(value) => {
+            handleText(value);
+          }}
+        />
+      </View>
+      <AppText>{description}</AppText>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        alignItems: 'center',
-        paddingHorizontal: 10,
-    },
-    currency:{
-        fontSize: 20, 
-        fontWeight: '900',
-        marginHorizontal:10,
-    },
-    input:{
-        fontSize: 20,
-        flex: 1,
-        fontWeight: '900',
-    },
-    subTitle:{ 
-        fontSize: 20,
-        fontWeight: '900',
-        marginBottom: 30,
-    },
-    title:{ 
-        fontSize: 25, 
-        fontWeight: '900',
-        marginBottom: 50,
-    },
+  container: {
+    // alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  currency: {
+    fontSize: 20,
+    fontWeight: "900",
+    marginHorizontal: 0,
+  },
+  input: {
+    fontSize: 20,
+    flex: 1,
+    fontWeight: "900",
+  },
+  subTitle: {
+    fontSize: 20,
+    fontWeight: "900",
+    marginBottom: 30,
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "900",
+    marginBottom: 50,
+    alignSelf: "center",
+  },
 });
 
 export default AmountInput;
--- app/screens/InvestmentPlanScreen.js
+++ app/screens/InvestmentPlanScreen.js
@@ -1,84 +1,135 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { View, StyleSheet, Alert } from 'react-native';
-import AmountInput from '../components/AmountInput';
-import Screen from '../components/Screen';
-import AppButton from '../components/AppButton';
+import { View, StyleSheet, Alert } from "react-native";
+import AmountInput from "../components/AmountInput";
+import Screen from "../components/Screen";
+import AppButton from "../components/AppButton";
+import PreferenceInvestmentOptionComponent from "../components/PreferenceInvestmentOptionComponent";
+import AppText from "../components/AppText";
 
 const items = [
-    { title: 'Education', subTitle: 'What is your target for this goal?', description:'You are setting up the total amount which you would like to achieve for Education.', keyId: 1,},
-    { title: 'Education', subTitle: 'What is your first investment?', description:'You can start from as low as PKR 500.', keyId: 2,},
-    { title: 'Education', subTitle: 'How much do you want to invest daily?', description:'This is the amount you will invest automantically on daily basis.', keyId: 3,},
-]
-
-
+  {
+    title: "Education",
+    subTitle: "What is your target for this goal?",
+    description:
+      "You are setting up the total amount which you would like to achieve.",
+    keyId: 1,
+  },
+  {
+    title: "Education",
+    subTitle: "What is your first investment?",
+    description: "You can start from as low as PKR 500.",
+    keyId: 2,
+  },
+  {
+    title: "Education",
+    subTitle: "How much do you want to invest daily?",
+    description: "This is the amount that will be invested on ",
+    keyId: 3,
+  },
+];
 
 function InvestmentPlanScreen({ navigation, route }) {
-    const [activeComponent, setActiveComponent] = useState(items[0]);
-    const [number, setNumber] =useState(null);
-    const [amount, setAmount]=useState([])
+  const [activeComponent, setActiveComponent] = useState(items[0]);
+  const [number, setNumber] = useState(null);
+  const [button, setButton] = useState({ value: "_____", id: 1 });
+  const [amount, setAmount] = useState([]);
 
-    console.log("InvestmentPlanScreen", route.params.title)
+  console.log("InvestmentPlanScreen", route.params);
 
-    useEffect(() => {
-        if(amount.length==3){
-            // navigation.navigate('PlannerScreen', amount)
-            setAmount([])
-            Alert.alert(
-                "Prompt message by SavvySave", 
-                "We need to asses your investment suitability.\nPlease answer some questions.", 
-                [
-                  { 
-                    text: "OK", 
-                    // onPress: () => console.log("OK Pressed") 
-                    onPress: ()=> {navigation.navigate('SuitabilityAssesmentScreen', amount)}
-                  }
-                ],
-              );
-        }
-    }, [amount]);
-
-
-    const handlePress = (keyId) => {
-        setActiveComponent(items[keyId])
-        setAmount([...amount, number]);
-        setNumber(null)
-        if(keyId==3){
-            setActiveComponent(items[0])
-        }
+  useEffect(() => {
+    if (amount.length == 3) {
+      // navigation.navigate('PlannerScreen', amount)
+      setAmount([]);
+      Alert.alert(
+        "Prompt message by SavvySave",
+        "We need to asses your investment suitability.\nPlease answer some questions.",
+        [
+          {
+            text: "OK",
+            // onPress: () => console.log("OK Pressed")
+            onPress: () => {
+              navigation.navigate("SuitabilityAssesmentScreen", {
+                amount,
+                button,
+              });
+            },
+          },
+        ]
+      );
     }
+  }, [amount]);
 
-    const handleAmount=(value)=>{
-        setNumber(value)
+  const handlePress = (keyId) => {
+    setActiveComponent(items[keyId]);
+    setAmount([...amount, number]);
+    setNumber(null);
+    if (keyId == 3) {
+      setActiveComponent(items[0]);
     }
+  };
+  console.log("Button from investmentPlanScreen ", button.value);
 
-    return (
-        <Screen>
-            <AmountInput
-                title={route.params.title}
-                subTitle={activeComponent.subTitle}
-                description={activeComponent.description}
-                initialValue={number}
-                onAmount={handleAmount}
+  const handleAmount = (value) => {
+    setNumber(value);
+  };
+
+  return (
+    <Screen>
+      <View style={styles.container}>
+        <AmountInput
+          title={route.params.goalName}
+          subTitle={activeComponent.subTitle}
+          description={activeComponent.description}
+          initialValue={number}
+          onAmount={handleAmount}
+        />
+        {activeComponent.keyId == 3 && (
+          <>
+            <AppText style={{ paddingLeft: 10 }}>{button.value} basis.</AppText>
+            <View
+              style={{
+                alignItems: "center",
+                marginTop: 10,
+                justifyContent: "space-between",
+              }}
+            >
+              <PreferenceInvestmentOptionComponent
+                onChange={(id, value) => setButton({ id, value })}
+              />
+            </View>
+          </>
+        )}
+        {number === null ||
+        (activeComponent.keyId === 3 && button.value === "_____") ? (
+          <></>
+        ) : (
+          <View style={styles.buttonContainer}>
+            <AppButton
+              title={"Continue"}
+              onPress={() => {
+                handlePress(activeComponent.keyId);
+              }}
             />
-            {
-                number === null?
-                    <></>
-                :
-                <View style={{flex: 1, justifyContent: 'flex-end'}}>
-                    <AppButton
-                        title={'Continue'}
-                        onPress={() => {handlePress(activeComponent.keyId)}}
-                    />
-                </View>
-            }
-
-        </Screen>
-    );
+          </View>
+        )}
+      </View>
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
-    container:{},
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    // position: "absolute",
+    // bottom: 20,
+    flexGrow: 1,
+    justifyContent: "flex-end",
+
+    paddingHorizontal: 20,
+  },
 });
 
 export default InvestmentPlanScreen;
